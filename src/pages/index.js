02352a9d6e519637595b11f270c8e36b9350606b
Kevin Bradwick <kevinbradwick@gmail.com>
2021-04-18 20:55:22 +0100

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
-import React, { PureComponent, useState } from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
-import {useWallet} from "@binance-chain/bsc-use-wallet";
+import { useWallet } from "@binance-chain/bsc-use-wallet";
 
 
 const IndexPage = () => {
   const [connected, setConnected] = useState(false);
-  // const [account, setAccount] = useState("");
-  const { account, connect, reset, status } = useWallet();
-
-  console.log(account);
+  const { account, connect } = useWallet();
   const handleConnect = (e) => {
     e.stopPropagation();
     connect('bsc');
@@ -16,17 +13,22 @@ const IndexPage = () => {
   };
 
   return <Layout>
-      <h1 className="govuk-heading-xl">My Company NFT's</h1>
+    <h1 className="govuk-heading-xl">My Company NFT's</h1>
+    {account &&
+      <div className="govuk-inset-text">Connected Account: <strong>{account}</strong></div>}
 
-      {account &&
-        <p>Connected with: {account}</p>}
+    {!connected &&
+      <button className="govuk-button" onClick={handleConnect}>Connect BSC Wallet</button>}
 
-      {!connected &&
-        <button onClick={handleConnect}>Connect</button>}
+    <h2 className="govuk-heading-m">Mint a new Company NFT</h2>
 
-    </Layout>
+    <hr className="govuk-section-break govuk-section-break--l govuk-section-break--visible" />
+
+    <h2 className="govuk-heading-m">My Company NFT's</h2>
+
+  </Layout>
 };
-  
+
 // https://docs.binance.org/smart-chain/wallet/wallet_api.html
 
 
